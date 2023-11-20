7db12e82e2a20488c0d87784c85ab3d6b178e6f8
fatih <alfatihrizqialfian@gmail.com>
2023-11-20 10:35:05 +0700

    change collection and doc

--- routes/data.js
+++ routes/data.js
@@ -22,33 +22,20 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const dataApp = db.collection('coba').doc('coba1');
+  const dataApp = db.collection('rental').doc('rent');
 
   const data = await dataApp.get();
-
-  //   fs.readFile('./formData.json', 'utf-8', (err, data) => {
-  //     if (err) {
-  //       res.status(500).send({ error: 'Error reading file' });
-  //     } else {
-  //       res.status(200).send(JSON.parse(data));
-  //     }
-  //   });
 
   res.status(200).send(data.data());
 });
 
 router.post('/submit', async (req, res) => {
   const formData = req.body;
-  //   console.log(formData);
+
   const response = await db
     .collection('coba')
     .doc('coba1')
-    .set(formData, { merge: true });
-
-  //   fs.writeFile('./formData.json', JSON.stringify(formData), (err) => {
-  //     if (err) throw err;
-  //     console.log('Data written to file');
-  //   });
+    .set(formData);
 
   res.status(200).send('Data berhasil di simpan!');
 });
